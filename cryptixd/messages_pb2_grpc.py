1b4909b92111a90e6cdec63e1a8d4ac4c70e2e22
Cryptix Network <info@king-krypto.de>
2025-01-08 16:49:26 +0100

    fix

--- cryptixd/messages_pb2_grpc.py
+++ cryptixd/messages_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-from . import messages_pb2 as messages__pb2
+import messages_pb2 as messages__pb2
 
 GRPC_GENERATED_VERSION = '1.63.0'
 GRPC_VERSION = grpc.__version__
--- server.py
+++ server.py
@@ -20,7 +20,7 @@ from cryptixd.CryptixdMultiClient import CryptixdMultiClient
 fastapi.logger.logger.setLevel(logging.WARNING)
 _logger = logging.getLogger(__name__)
 
-# FastAPI-Anwendung erstellen
+
 app = FastAPI(
     title="Cryptix REST-API server",
     description="This server is to communicate with Cryptix Network via REST-API",
@@ -29,7 +29,7 @@ app = FastAPI(
     license_info={"name": "MIT LICENSE"},
 )
 
-# Middleware hinzufügen
+
 app.add_middleware(GZipMiddleware, minimum_size=500)
 app.add_middleware(LimitUploadSize, max_upload_size=200_000)  # ~1MB
 
@@ -43,7 +43,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Models für die Antwort
+
 class CryptixdStatus(BaseModel):
     is_online: bool = False
     server_version: Optional[str] = None
@@ -68,7 +68,7 @@ async def ping_server():
     result = PingResponse()
     error = False
 
-    # Überprüfung der Cryptixd-Client-Verbindung
+
     try:
         info = await cryptixd_client.cryptixds[0].request("getInfoRequest")
         result.cryptixd.is_online = True
@@ -79,43 +79,43 @@ async def ping_server():
         _logger.error("Cryptixd health check failed: %s", err)
         error = True
 
-    # Überprüfung der Datenbankverbindung
+
     if os.getenv("SQL_URI") is not None:
         async with async_session() as session:
             try:
-                # SQL-Abfrage korrekt ausführen mit text()
+
                 await session.execute(text("SELECT 1"))
                 result.database.is_online = True
             except Exception as err:
                 _logger.error("Database health check failed: %s", err)
                 error = True
 
-    # Wenn ein Fehler aufgetreten ist oder Cryptixd nicht synchronisiert ist
+
     if error or not result.cryptixd.is_synced:
         return JSONResponse(status_code=500, content=result.dict())
 
     return result
 
 
-# Einrichten der Cryptixd-Hosts
+
 cryptixd_hosts = []
 
-# Versuche, bis zu 100 Cryptixd-Hosts zu laden
+
 for i in range(100):
     try:
         cryptixd_hosts.append(os.environ[f"CRYPTIXD_HOST_{i + 1}"].strip())
     except KeyError:
         break
 
-# Wenn keine Hosts gesetzt sind, wird eine Ausnahme geworfen
+
 if not cryptixd_hosts:
     raise Exception("Please set at least CRYPTIXD_HOST_1 environment variable.")
 
-# CryptixdClient initialisieren
+
 cryptixd_client = CryptixdMultiClient(cryptixd_hosts)
 
 
-# Ausnahmebehandlung für alle unerwarteten Fehler
+
 @app.exception_handler(Exception)
 async def unicorn_exception_handler(request: Request, exc: Exception):
     await cryptixd_client.initialize_all()
@@ -128,7 +128,7 @@ async def unicorn_exception_handler(request: Request, exc: Exception):
     )
 
 
-# Periodische Initialisierung des Clients bei Server-Start
+
 @app.on_event("startup")
 @repeat_every(seconds=60)
 async def periodical_blockdag():
